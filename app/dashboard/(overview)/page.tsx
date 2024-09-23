fa26c04ab9bf3dd4cf8a550e1a41e2cec63c79f1
Silvio Franco <silvioafranco@outlook.com>
2024-09-23 15:04:29 -0300

    Pages created with PPR and Pagination

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,32 @@
+import RevenueChart from "../../ui/dashboard/revenue-chart";
+import LatestInvoices from "../../ui/dashboard/latest-invoices";
+import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
+import { lusitana } from "../../ui/fonts";
+import { fetchLatestInvoices } from "../../lib/data";
+import { Suspense } from "react";
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from "@/app/ui/skeletons";
+
+export default async function Page() {
+    const latestInvoices = await fetchLatestInvoices();
+
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2x1`}>
+                Dashboard
+                                                                                                                  </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <Suspense fallback={<CardsSkeleton />}>
+                    <CardWrapper />
+                </Suspense>
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
+            </div>
+        </main>
+    );
+}
