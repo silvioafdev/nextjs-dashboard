f4c1c3432e079f191b053fc5bff1b599db5ae7ad
Silvio Franco <silvioafranco@outlook.com>
2024-09-24 14:53:59 -0300

    Project done

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,13 +2,16 @@ import RevenueChart from "../../ui/dashboard/revenue-chart";
 import LatestInvoices from "../../ui/dashboard/latest-invoices";
 import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
 import { lusitana } from "../../ui/fonts";
-import { fetchLatestInvoices } from "../../lib/data";
 import { Suspense } from "react";
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from "@/app/ui/skeletons";
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+    title: 'Invoices',
+  };
+
 
 export default async function Page() {
-    const latestInvoices = await fetchLatestInvoices();
-
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2x1`}>
